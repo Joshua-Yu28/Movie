8090b878e4fb38aeacb5e8b2905cbc3892cfea56
JoshuaY <yu.zhuoc@northeastern.edu>
2024-05-16 19:49:31 +0800

    5

--- MovieClient/movie-gold-v1/src/components/hero/Hero.js
+++ MovieClient/movie-gold-v1/src/components/hero/Hero.js
@@ -1,23 +1,37 @@
 import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 const Hero = ({movies}) => {
     return (
         <div className='movie-carousel-container'>
             <Carousel>
                 {
+                    //Uses optional chaining to prevent errors. If movies is undefined or null, 
+                    //it returns undefined and does not attempt to call map.
                     movies?.map((movie) =>{
                         return (
                             <Paper>
                                 <div className='movie-card-container'>
-                                    <div className='movie-card'>
+                                    <div className='movie-card' style={{"--img": `url(${movie.backdrops[0]})`}}>
                                         <div className='movie-detail'>
                                             <div className='movie-poster'>
                                                 <img src={movie.poster} alt='' />
                                             </div>
                                             <div className='movie-title'>
                                                 <h4>{movie.title}</h4>
+                                            </div>
+                                            <div className='movie-buttons-container'>
+                                                <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                                                <div className="play-button-icon-container">
+                                                    <FontAwesomeIcon className="play-button-icon"
+                                                        icon = {faCirclePlay}
+                                                    />
+
+                                                </div>
+                                                </Link>
                                             </div>
                                         </div>
                                     </div>
